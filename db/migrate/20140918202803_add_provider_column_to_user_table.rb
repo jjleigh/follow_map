a16d900708e5e9def853283932db583d076c4513
Justine Jones <justinejones91@gmail.com>
2014-09-18 18:18:51 -0400

    ran migrations to add the uid and provider columns to the user table

---
+++ db/migrate/20140918202803_add_provider_column_to_user_table.rb
@@ -0,0 +1,5 @@
+class AddProviderColumnToUserTable < ActiveRecord::Migration
+  def change
+  	add_column :users, :provider, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918143603) do
+ActiveRecord::Schema.define(version: 20140918203054) do
 
   create_table "followers", force: true do |t|
     t.string   "name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20140918143603) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
 end
