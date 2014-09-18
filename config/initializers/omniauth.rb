9c63f13eb0790287706bbc12948998cb35e94641
Justine Jones <justinejones91@gmail.com>
2014-09-18 18:19:31 -0400

    added the provider key setup

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+	provider :google, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+end
