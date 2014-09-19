148e0be4c5fa9e4fe7f8b2802fbe8c28e25403bb
Justine Jones <justinejones91@gmail.com>
2014-09-18 21:04:59 -0400

    moved script tage to the top"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	has_many :friends
-	validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation
+	# validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation
 
 
 	def self.create_with_omniauth(auth)
 		create! do |user|
 			user.provider = auth[:provider]
 			user.uid = auth[:uid]
+			# user.first_name = auth[:first_name]
+			# user.last_name = auth[:last_name]
+			# user.email = auth[:email]
 		end
 		
 	end
